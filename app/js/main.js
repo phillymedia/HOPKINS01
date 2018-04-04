8b8d6bcefef916d1f7f2f4185c7fc6939efd88de
jared-whalen <jwhalen@philly.com>
2018-04-04 11:25:25 -0400

    Changed #intro-container positioning

--- app/js/main.js
+++ app/js/main.js
@@ -2,16 +2,20 @@ require("./video-strip.js")
 
 $(document).ready(function() {
 
-  var h = $(".header").height(),
-    w = $(window).height(),
-    ci = parseInt($(".content.is-subscriber").css("padding-top")),
-    wh = w - h;
-  console.log(ci)
-  $("#intro-container").css({
-    "height": wh,
-    "margin-top": (h - ci)
+  $(window).on("load resize", function(e) {
+
+    var h = $(".header").height(),
+      w = $(window).height(),
+      ci = parseInt($(".content.is-subscriber").css("padding-top")),
+      wh = w - h;
+    console.log(ci)
+    $("#intro-container").css({
+      "height": wh,
+      "margin-top": (h - ci)
+    });
+    $("#intro-images").css("height", wh);
+
   });
-  $("#intro-images").css("height", wh);
 
   setTimeout(function() {
     $("#intro-clean").fadeIn(2000);
