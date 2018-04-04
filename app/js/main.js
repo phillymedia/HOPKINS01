772a486af18f65220c468d5f191e38b433918013
jared-whalen <jwhalen@philly.com>
2018-04-04 17:09:18 -0400

    Restored version

--- app/js/main.js
+++ app/js/main.js
@@ -1,28 +1,27 @@
 $(document).ready(function() {
 
   if ($(window).width() < 800) {
-    $("#item-0").appendTo("#item-0-target");
+     $("#item-0").appendTo("#item-0-target");
   }
 
+
   $(window).on("load resize", function(e) {
-    $('.max-width-video-wrapper').each(function() {
-      this.style.setProperty('max-width', '100%', 'important');
-    });
+
     var h = $(".header").height(),
       w = $(window).height(),
       ci = parseInt($(".content.is-subscriber").css("padding-top")),
       wh = w - h;
-
     $("#intro-container").css({
       "height": wh,
       "margin-top": (h - ci)
     });
     $("#intro-images").css("height", wh);
-    var iiw = $("#intro-images").width();
-    $("#bkgimgc").css("width", iiw);
+
   });
 
-
+  setTimeout(function() {
+    $("#intro-clean").fadeIn(2000);
+  }, 1000);
 
   // $(window).scroll(function() {
   //   var top = $(window).scrollTop();
